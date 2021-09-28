2db68fca9e8fe0b3fe2d67f3a30497d7805087ff
Arjun  Dass <ad.sam007@gmail.com>
2021-09-28 13:56:28 -0700

    dependency injections and qualifiers

--- SpringAnnotations/src/com/spring/springannotations/TennisCoach.java
+++ SpringAnnotations/src/com/spring/springannotations/TennisCoach.java
@@ -1,17 +1,41 @@
 package com.spring.springannotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements  Coach {
 
+    @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
-    @Autowired
-    public TennisCoach(FortuneService theFortuneService) {
-        fortuneService = theFortuneService;
+    //define a default constructor
+    public TennisCoach() {
+        System.out.println(">> Tennis Coach: inside default constructor");
     }
+
+//    // define a setter method
+//    @Autowired
+//    public void setFortuneService(FortuneService theFortuneService) {
+//        System.out.println(">> Tennis Coach: inside setter method");
+//        fortuneService = theFortuneService;
+//    }
+
+//    // define a method
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService theFortuneService) {
+//        System.out.println(">> Tennis Coach: inside doSomeCrazyStuff method");
+//        fortuneService = theFortuneService;
+//    }
+
+//    @Autowired
+//    public TennisCoach(FortuneService theFortuneService) {
+//        fortuneService = theFortuneService;
+//    }
+
+    // Field Injection
 
     @Override
     public String getDailyWorkout() {
