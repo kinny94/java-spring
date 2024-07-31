8972aa0adb37435191a2649d032459315ed94d5a
Arjun Dass <ad.sam007@gmail.com>
2024-07-30 23:18:32 -0700

    autwired

--- demoapp3/src/main/java/com/dependency/demo/rest/DemoController.java
+++ demoapp3/src/main/java/com/dependency/demo/rest/DemoController.java
@@ -11,8 +11,8 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    DemoController(Coach theCoach) {
-        myCoach = theCoach;
+    public void setCoach(Coach coach) {
+        myCoach = coach;
     }
 
     @GetMapping("/dailyWorkout")
