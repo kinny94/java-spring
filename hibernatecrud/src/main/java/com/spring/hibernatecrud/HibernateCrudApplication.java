e16e36f561d9ec0908439fee886a16683855a0e5
Arjun Dass <ad.sam007@gmail.com>
2024-08-05 20:55:41 -0700

    hibernate crud initialization

---
+++ hibernatecrud/src/main/java/com/spring/hibernatecrud/HibernateCrudApplication.java
@@ -0,0 +1,19 @@
+package com.spring.hibernatecrud;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HibernateCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateCrudApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> System.out.println("Hello World!");
+	}
+}
