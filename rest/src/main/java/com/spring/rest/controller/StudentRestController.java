42e9916d084ffb3437a77b614de8eaacc6fc705b
Arjun Dass <ad.sam007@gmail.com>
2024-08-07 16:49:48 -0700

    add path variable end point

--- rest/src/main/java/com/spring/rest/controller/StudentRestController.java
+++ rest/src/main/java/com/spring/rest/controller/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.spring.rest.controller;
 
 import com.spring.rest.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +14,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void init() {
+        students = new ArrayList<>();
         students.add(new Student("Anthony", "Edwards", "anthony@edward.com"));
         students.add(new Student("Kevin", "Durant", "Kevin@durant.com"));
         students.add(new Student("Jayson", "Tatum", "Jason@Tatum.com"));
         students.add(new Student("Jaylen", "Brown", "Jaylen@brown.com"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/student/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
