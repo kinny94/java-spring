8e60699781c18ccf12ab0f5ab134e656d3a53388
Arjun Dass <ad.sam007@gmail.com>
2024-08-07 17:21:09 -0700

    global exception handling

--- rest/src/main/java/com/spring/rest/controller/StudentRestController.java
+++ rest/src/main/java/com/spring/rest/controller/StudentRestController.java
@@ -36,23 +36,5 @@ public class StudentRestController {
         }
         return students.get(studentId);
     }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
-        StudentErrorResponse response = new StudentErrorResponse();
-        response.setStatus(HttpStatus.NOT_FOUND.value());
-        response.setMessage(e.getMessage());
-        response.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND) ;
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
-        StudentErrorResponse response = new StudentErrorResponse();
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
-        response.setMessage(e.getMessage());
-        response.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
 }
 
