3b7f385061f3bf04e7196cc6661c804000c622c7
Arjun Dass <ad.sam007@gmail.com>
2024-08-14 23:18:54 -0700

    adding rest-data dependency

--- app/src/main/java/com/springboot/app/dao/EmployeeRepository.java
+++ app/src/main/java/com/springboot/app/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.springboot.app.dao;
 
 import com.springboot.app.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // This is equivalent of creating your own dao and dao-impl
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
