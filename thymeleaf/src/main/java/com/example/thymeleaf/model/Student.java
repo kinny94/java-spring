935c717a89ea807ef92316bd833ecdb8a993765c
Arjun Dass <ad.sam007@gmail.com>
2024-08-28 11:57:21 -0700

    radio buttons in thymeleaf

--- thymeleaf/src/main/java/com/example/thymeleaf/model/Student.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/model/Student.java
@@ -5,6 +5,15 @@ public class Student {
     private String firstName;
     private String lastName;
     private String country;
+    private String favoriteLanguage;
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
 
     public String getFirstName() {
         return firstName;
