d258444a6a76c9306a7cef2520afd9f5c4ae1b41
Arjun Dass <ad.sam007@gmail.com>
2024-08-28 13:33:00 -0700

    max min and regex validation

--- form-validation/src/main/java/com/validation/mvc/Customer.java
+++ form-validation/src/main/java/com/validation/mvc/Customer.java
@@ -1,15 +1,38 @@
 package com.validation.mvc;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
     private String firstName;
 
+    @Min(value = 0, message = "must be greater than or equal to 0")
+    @Max(value=10, message="can't be greater than 10")
+    private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters")
+    private String postalCode;
+
     @NotNull(message = "is required")
     @Size(min=1, message = "is required")
     private String lastName = "";
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
